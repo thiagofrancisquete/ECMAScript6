3df39c0e240c197ecbd18a96b02b41cfac704d36
thiagofrancisquete <thiago.francisquete@gmail.com>
2018-01-16 19:12:01 -0200

    create a template string

---
+++ template string/intro.js
@@ -0,0 +1,6 @@
+const login = 'ecmascript'
+const dia = '16 de janeiro'
+const ano = 2016
+
+const mensagem = 'olá ' +login+ '!\nHoje é ' +dia+ ' de ' +ano
+console.log(mensagem)
---
+++ template string/templateString.js
@@ -0,0 +1,15 @@
+// var nome = 'Lucas'
+// console.log('bem vindo '+nome)
+
+const nome = 'João'
+console.log(`Bem vindo, ${nome}`)
+
+var n1 = 1
+var n2 = 3
+console.log(`o resultado de ${n1} + ${n2} é ${n1 + n2}`)
+// console.log('exibindo\numa\npalavra\npor\nlinha)
+console.log(`exibindo
+uma
+palavra
+por
+linha`)
