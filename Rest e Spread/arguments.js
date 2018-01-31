04e0d178d6e65fe86a37b9e9b30f155112649a40
thiagofrancisquete <thiago.francisquete@gmail.com>
2018-01-31 15:43:39 -0200

    using arguments

---
+++ Rest e Spread/arguments.js
@@ -0,0 +1,12 @@
+function somaTudo(){
+    var soma = 0
+    for (var i = 0; i < arguments.length; i++){
+        soma += arguments[i]
+    }
+
+    console.log(soma)
+}
+
+somaTudo(1)
+somaTudo(1,2,3)
+somaTudo(1,2)
