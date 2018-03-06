e41ada1a796bb7de7f91e9fe7590426fb4d8f7cc
thiagofrancisquete <thiago.francisquete@gmail.com>
2018-03-06 10:34:30 -0300

    using methods in Number

---
+++ Numbers/isFinite.js
@@ -0,0 +1,8 @@
+console.log(isFinite(10));
+console.log(isFinite(NaN));
+console.log(isFinite(null));
+console.log(isFinite([]));
+console.log(Number.isFinite(10));
+console.log(Number.isFinite(NaN));
+console.log(Number.isFinite(null));
+console.log(Number.isFinite([]));
