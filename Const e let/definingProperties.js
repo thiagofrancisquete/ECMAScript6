e5581187ebbdf6c8690ac093143f6e6cc891d37b
thiagofrancisquete <thiago.francisquete@gmail.com>
2018-03-06 10:01:06 -0300

    defining propeties using let

---
+++ Const e let/definingProperties.js
@@ -0,0 +1,16 @@
+/* com es5 
+var x = 1, y = 2;
+var object = {
+  x: x,
+  y: y
+};
+console.log(`${object.x}`);
+*/
+
+let x = 1,
+  y = 2;
+let object = {
+  x,
+  y
+};
+console.log(`${object.x}`);
