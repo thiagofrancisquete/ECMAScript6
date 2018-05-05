c68d5c46b95d9962ad7b5f294306d9050fff415d
thiagofrancisquete <thiago.francisquete@gmail.com>
2018-05-05 18:39:50 -0300

    using concat function

---
+++ Array - Métodos auxiliares/concat.js
@@ -0,0 +1,6 @@
+const listaMae = ['leite', 'arroz', 'feijao']
+const listaTia = ['macarrao', 'abobora', 'queijo']
+const listaEscritorio = ['papel A4', 'caneta', 'grampo']
+
+const listaJunta = listaMae.concat(listaTia, listaEscritorio)
+console.log(listaJunta)
