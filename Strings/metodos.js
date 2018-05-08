0c8ddd4da50e74c3c612d7b216b47067fdaf1f81
thiagofrancisquete <thiago.francisquete@gmail.com>
2018-05-08 10:25:09 -0300

    put folder template string in string folder

---
+++ Strings/metodos.js
@@ -0,0 +1,7 @@
+console.log(`\uD83D\uDE91`.codePointAt(1))
+console.log(String.fromCodePoint(0x61, 0x62, 0x63));
+console.log(`a`.repeat(6));
+var str = 'Hi I am a JS developer';
+console.log(str.includes("JS"));
+console.log(str.startsWith('JS', 13));
+console.log(str.endsWith('r'));
