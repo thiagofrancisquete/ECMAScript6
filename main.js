5e780b71ab6d8bc56648d47d454288c672b9f7e7
thiagofrancisquete <thiago.francisquete@gmail.com>
2018-07-12 02:23:00 -0300

    aula 01 - classes

---
+++ main.js
@@ -0,0 +1,32 @@
+/* AULA 01 - CLASES
+class List {
+  constructor() {
+    this.data = [];
+  }
+
+  add(data) {
+    this.data.push(data);
+    console.log(this.data);
+  }
+}
+
+class TodoList extends List {
+  constructor() {
+    super();
+    this.usuario = 'Thiago';
+  }
+
+  mostraUser() {
+    console.log(this.usuario);
+  }
+}
+
+const MinhaLista = new TodoList();
+
+document.getElementById('novotodo').onclick = function() {
+  MinhaLista.add('Novo todo');
+}
+
+MinhaLista.mostraUser();
+*/
+
