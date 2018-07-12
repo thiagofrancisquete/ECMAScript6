fd7e838b2289101328770b968b05684adec0d02c
thiagofrancisquete <thiago.francisquete@gmail.com>
2018-07-12 13:30:40 -0300

    metodos do array: find, reduce, filter e map

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-/* AULA 01 - CLASES
+// AULA 01 - CLASES
+/*
 class List {
   constructor() {
     this.data = [];
@@ -30,3 +31,32 @@ document.getElementById('novotodo').onclick = function() {
 MinhaLista.mostraUser();
 */
 
+// Aula 3 - OPerações em array
+const arr = [1, 3, 4, 5, 8, 9];
+
+// map = percorre todo o arrayinvocando uma função de retorno em cada elemento com parâmetros. 
+// Use map() quando: é preciso traduzir/mapear todos os elementos em um array para outro conjunto de valores.
+const newArr = arr.map(function (item, index) {
+  return item * index;
+})
+console.log(newArr);
+
+// reduce = consumir todo o vetor e transformar num unico dado, normalmente um numero
+// Use reduce() quando: é preciso encontrar um valor cumulativo ou concatenado com base em elementos de todo o array.
+const sum = arr.reduce(function(total, next){
+  return total + next;
+});
+console.log(sum);
+
+// filter = retorna o array com os elementros true do filtro
+// Use filter() quando: é preciso remover elementos indesejados com base em alguma(s) condição(ões)
+const filtro = arr.filter(function(item){
+  return item % 2 === 0;
+})
+console.log(filtro);
+
+// find = usado para procurar se x existe no array
+const procurar = arr.find(function(item){
+  return item === 6;
+})
+console.log(procurar);
